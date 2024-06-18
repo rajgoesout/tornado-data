7b3584867ed2df9d5d4fc5c0782a25b52ebc1643
Raj <rajgoesout@gmail.com>
2024-06-18 18:46:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -10,43 +10,75 @@ freq100to1k = json.load(open("freq100to1k.json"))["EVM"]["Calls"]
 freq1kto10k = json.load(open("freq1kto10k.json"))["EVM"]["Calls"]
 freq10kto100k = json.load(open("freq10kto100k.json"))["EVM"]["Calls"]
 
-freqsenderset100  = set()
+freqsenderset100 = set()
 freqsenderset100to1k = set()
 freqsenderset1kto10k = set()
 freqsenderset10kto100k = set()
 freqsenderset = set()
 
 for i in freq100:
-  freqsenderset100.add(i["Transaction"]["From"])
-  freqsenderset.add(i["Transaction"]["From"])
+    freqsenderset100.add(i["Transaction"]["From"])
+    freqsenderset.add(i["Transaction"]["From"])
 for i in freq100to1k:
-  freqsenderset100to1k.add(i["Transaction"]["From"])
-  freqsenderset.add(i["Transaction"]["From"])
+    freqsenderset100to1k.add(i["Transaction"]["From"])
+    freqsenderset.add(i["Transaction"]["From"])
 for i in freq1kto10k:
-  freqsenderset1kto10k.add(i["Transaction"]["From"])
-  freqsenderset.add(i["Transaction"]["From"])
+    freqsenderset1kto10k.add(i["Transaction"]["From"])
+    freqsenderset.add(i["Transaction"]["From"])
 for i in freq10kto100k:
-  freqsenderset10kto100k.add(i["Transaction"]["From"])
-  freqsenderset.add(i["Transaction"]["From"])
+    freqsenderset10kto100k.add(i["Transaction"]["From"])
+    freqsenderset.add(i["Transaction"]["From"])
 
-def find_correlation(top_txns, freq_senders):
+
+def find_correlation(top_txns, freq_senders, freq_senders_list):
     total = 0
     ftd = {}
     for txn in top_txns:
-        total += float(txn["sum"])
+        total += float(txn["Transaction"]["ValueInUSD"])
         if txn["Transaction"]["From"] in freq_senders:
             if txn["Transaction"]["From"] not in ftd:
-                ftd[txn["Transaction"]["From"]] = (float(txn["sum"]), 1)
+                ftd[txn["Transaction"]["From"]] = (
+                    float(txn["Transaction"]["ValueInUSD"]),
+                    1,
+                )
             else:
-                ftd[txn["Transaction"]["From"]] = (ftd[txn["Transaction"]["From"]][0]+float(txn["sum"]), ftd[txn["Transaction"]["From"]][1]+1)
+                ftd[txn["Transaction"]["From"]] = (
+                    ftd[txn["Transaction"]["From"]][0]
+                    + float(txn["Transaction"]["ValueInUSD"]),
+                    ftd[txn["Transaction"]["From"]][1] + 1,
+                )
         if txn["Transaction"]["To"] in freq_senders:
             if txn["Transaction"]["To"] not in ftd:
-                ftd[txn["Transaction"]["To"]] = (float(txn["sum"]), 1)
+                ftd[txn["Transaction"]["To"]] = (
+                    float(txn["Transaction"]["ValueInUSD"]),
+                    1,
+                )
             else:
-                ftd[txn["Transaction"]["To"]] = (ftd[txn["Transaction"]["To"]][0]+float(txn["sum"]), ftd[txn["Transaction"]["To"]][1]+1)
-    return (total, len(ftd), ftd)
+                ftd[txn["Transaction"]["To"]] = (
+                    ftd[txn["Transaction"]["To"]][0]
+                    + float(txn["Transaction"]["ValueInUSD"]),
+                    ftd[txn["Transaction"]["To"]][1] + 1,
+                )
+    ftotal = 0
+    ftxn_count = 0
+    for sender in freq_senders_list:
+        ftotal += float(sender["Transaction"]["ValueInUSD"])
+        ftxn_count += float(sender["tx_count"])
+    return (
+        total,
+        ftotal,
+        ftxn_count,
+        len(ftd),
+        len(top_txns),
+        100 * len(ftd) / len(top_txns),
+        ftd,
+    )
 
-print("ftd100", find_correlation(top100, freqsenderset100)[:2])
-print("ftd100to1k", find_correlation(top100to1k, freqsenderset100to1k)[:2])
-print("ftd1kto10k", find_correlation(top1kto10k, freqsenderset1kto10k)[:2])
-print("ftd10kto100k", find_correlation(top10kto100k, freqsenderset10kto100k)[:2])
+
+print("ftd100", find_correlation(top100, freqsenderset100, freq100)[:6])
+print("ftd100to1k", find_correlation(top100to1k, freqsenderset100to1k, freq100to1k)[:6])
+print("ftd1kto10k", find_correlation(top1kto10k, freqsenderset1kto10k, freq1kto10k)[:6])
+print(
+    "ftd10kto100k",
+    find_correlation(top10kto100k, freqsenderset10kto100k, freq10kto100k)[:6],
+)
